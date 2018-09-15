a19a40c098ab3c3574ae9792e99dfa2b32e1d0fd
miyass <exe.miyakura0629@gmail.com>
2018-09-15 11:41:08 +0900

    change UIButton

--- CountVer2/ViewController.swift
+++ CountVer2/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Multiple(_ sender: UIButton) {
+        let buttonText = sender.currentTitle!
+        let buttonNumber = Int(buttonText)
+        multi(number: buttonNumber!)
+    }
+    
+    func multi(number: Int) {
+        var labelNumber = Int(resultLabel.text!)
+        labelNumber = labelNumber! * number
+        resultLabel.text = String(labelNumber!)
+    }
+    
 
 }
 
